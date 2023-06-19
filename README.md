d8d2c4f7bcced3bc558735910b718071ecb490be
kango <kangoyoshioka@gmail.com>
2023-06-20 02:18:34 +0900

    高速化

---
+++ README.md
@@ -0,0 +1,17 @@
+# CUDAレイトレーシングレンダラー
+
+CUDAを用いたパストレーサーです
+
+# DEMO
+
+![sample.png](sample.png)
+![sample2.png](sample2.png)
+
+# Features
+
+CUDAを用いたパストレーサーで高速なレンダリングを可能にしています
+# Requirement
+
+* vcpkg
+* cmake
+* msvc
--- src/main.cu
+++ src/main.cu
@@ -92,7 +92,75 @@ void func1() {
     cudaDeviceReset();
 }
 
+void func2() {
+    const auto floor_color = codeToColor("#f9c89b");
+    const double room_r = 1e5;
+    const double room_kd = 0.8;
+    Sphere roomSpheres[] = {
+            Sphere(room_r, Eigen::Vector3d{room_r - 40, 0, 0}),
+            Sphere(room_r, Eigen::Vector3d{-(room_r - 40), 0, 0}),
+            Sphere(room_r, Eigen::Vector3d{0, 0, room_r - 100}),
+            Sphere(room_r, Eigen::Vector3d{0, room_r - 50, 0}),
+            Sphere(room_r, Eigen::Vector3d{0, -(room_r - 50), 0})
+    };
+
+    Body room[] = {
+            Body(0.0, Material(M_DIFFUSE, codeToColor("#2f5d50"), room_kd), roomSpheres[0]),
+            Body(0.0, Material(M_DIFFUSE, codeToColor("#00a3af"), room_kd), roomSpheres[1]),
+            Body(0.0, Material(M_DIFFUSE, floor_color, room_kd), roomSpheres[2]),
+            Body(0.0, Material(M_DIFFUSE, floor_color, room_kd), roomSpheres[3]),
+            Body(0.0, Material(M_DIFFUSE, floor_color, room_kd), roomSpheres[4]),
+    };
+
+    std::vector<Body> bodies {
+            Body(25, Material(codeToColor("#e597b2"), 1.0, 0.0, 0.0), Sphere(10, Eigen::Vector3d(0, 40, 0))),
+            Body(0.0, Material(Color(0.75, 0.25, 0.25), 0.8, 0.0, 0.0), Sphere(12, Eigen::Vector3d(15, -38, -30))),
+            Body(0.0, Material(codeToColor("#e5e4e6"), 0.8, 0.0, 0.0), Sphere(15, Eigen::Vector3d(-18, -35, -45))),
+            Body(0.0, Material(codeToColor("#b0c4de"), 0.3, 0.5, 0.0), Sphere(8, Eigen::Vector3d(-3, -42, 5))),
+            Body(0.0, Material(codeToColor("#c75595"), 0.6, 0.01, 0.0), Sphere(8, Eigen::Vector3d(-15, -42, -10))),
+            Body(0.0, Material(codeToColor("#bdb76b"), 0.3, 0.2, 0.2), Sphere(9, Eigen::Vector3d(12, -41, -3))),
+    };
+
+    for(int i = 0; i < 5; i++) {
+        bodies.emplace_back(room[i]);
+    }
+
+    Eigen::Vector3d sum = Eigen::Vector3d::Zero();
+    for(int i = 1; i < 6; ++i) sum += bodies[i].getSphere().center;
+    const Eigen::Vector3d camOrg{0, 5, 100.0};
+    const Eigen::Vector3d camdir = sum / 5.0 - camOrg;
+    const Camera camera(
+            camOrg,
+            camdir,
+            1920, 9.0 / 16.0, 50, 1.0, camdir.norm(), 0.5, 20
+    );
+
+    Scene scene(bodies.size(), camera, bodies.data(), Color::Zero());
+
+    scene.printSceneInfo();
+
+    LARGE_INTEGER freq;
+    QueryPerformanceFrequency(&freq);
+
+    cudaDeviceSynchronize();
+
+    LARGE_INTEGER start, end;
+    QueryPerformanceCounter(&start);
+
+    auto image = generateImageWithGPU(scene, 1e4).apply_reinhard_extended_tone_mapping().apply_bilateral_filter(5, 150, 10).apply_gamma_correction();
+
+    QueryPerformanceCounter(&end);
+
+    const double time = static_cast<double>(end.QuadPart - start.QuadPart) / freq.QuadPart;
+    std::cout << "Generation time\t" << time << " [sec]" << std::endl;
+
+    // image.show();
+    image.save("../../Results/sample2.png");
+
+    cudaDeviceReset();
+}
+
 int main() {
-    func1();
+    func2();
     return 0;
 }
